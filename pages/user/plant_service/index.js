eea4c5a95f6e58b600fa7e1a851b0c2eb773ee24
5200016 <1617318433@qq.com>
2019-05-24 17:38:10 +0800

    项目更新

--- pages/user/plant_service/index.js
+++ pages/user/plant_service/index.js
@@ -1,66 +1,111 @@
-// pages/user/plant_service/index.js
+const app = getApp();
+const util = require("../../../utils/util.js");
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        url: app.globalData.HOST + '/',
+        hidden: false,
+        pageNum: 1,
+        pageSize: 5,
+        totalPages: 0,
+        totalElements: 0,
+        orderServiceList: [],
+        scrollHeight: 0,
+        hiddenMore: true,
+    },
 
-  },
+    getOrderServiceList: function(){
+        let url = '/order/service';
+        let data = {
+            userId: wx.getStorageSync("userId"),
+            pageNum: this.data.pageNum - 1,
+            pageSize: this.data.pageSize,
+        };
+        app.wxRequest('GET', url, data, (res) => {
+            if (res.result) {
+                let data = this.data.orderServiceList;
+                for (let i = 0; i < res.data.content.length; i++){
+                    data.push(res.data.content[i]);
+                }
+                this.setData({
+                    hidden: true,
+                    orderServiceList: data,
+                    totalPages: res.data.totalPages,
+                    totalElements: res.data.totalElements
+                });
+            } else {
+                app.optionToast(res.msg);
+            }
+        }, (err) => {
+            console.log(err.data);
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+        });
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        let that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    scrollHeight: res.windowHeight
+                });
+            }
+        });
+        this.getOrderServiceList();
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  }
+    }
 })
